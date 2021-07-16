40a9c2eac8bfde8b532c3f8565cc7777027d103b
洪瑛蔚 <4a33a006@stust.edu.tw>
2021-07-16 14:10:53 +0800

    start set up

---
+++ pages/index.vue
@@ -0,0 +1,22 @@
+<template>
+ <div>
+   <button class="m-16" @click="getData">get api</button>
+ </div>
+</template>
+
+<script>
+export default {
+methods: {
+  // getData () {
+  //   const api = ''
+  // }
+
+}
+}
+</script>
+
+<style>
+button{
+  border: 3px solid black;
+}
+</style>
