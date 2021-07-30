bb31a40d90f46a5bcbbfb2e2463960a8f3fd0bfe
Ying Wei hung <4a33a006@stust.edu.tw>
2021-07-30 15:59:19 +0800

    heroku fix

--- nuxt.config.js
+++ nuxt.config.js
@@ -46,10 +46,10 @@ export default {
     '@nuxtjs/tailwindcss',
     '@nuxtjs/axios'
   ],
-  // server: {     
-  //   port: 3000, // default: 3000     
-  //   host: '0.0.0.0', // default: localhost   
-  // },
+  server: {     
+    port: 3000, // default: 3000     
+    host: '0.0.0.0', // default: localhost   
+  },
 
   // Modules: https://go.nuxtjs.dev/config-modules
   modules: [
