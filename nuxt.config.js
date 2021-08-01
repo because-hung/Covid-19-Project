03ef692dfc74a5a62d2744c6b3874cb411ec06c8
Ying Wei hung <4a33a006@stust.edu.tw>
2021-08-02 05:47:26 +0800

    fix

--- nuxt.config.js
+++ nuxt.config.js
@@ -45,8 +45,8 @@ export default {
     '@nuxtjs/axios'
   ],
   server: {     
-    port: 3000, // default: 3000     
-    host: 'localhost', // default: localhost   
+    port: 8080, // default: 3000     
+    host: '0.0.0.0', // default: localhost   
   },
 
   // Modules: https://go.nuxtjs.dev/config-modules
