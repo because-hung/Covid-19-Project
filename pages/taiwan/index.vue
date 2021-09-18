9bc349d33b20ffd678dc69ee12894221411bbb1c
Ying Wei hung <4a33a006@stust.edu.tw>
2021-09-19 00:30:05 +0800

    taiwan country page ok

--- pages/taiwan/index.vue
+++ pages/taiwan/index.vue
@@ -1,8 +1,11 @@
 <template>
   <div>
     <div class="Taiwntitle flex justify-between font-bold mt-8 mb-10">
-   <h2 class="ml-2 pb-1 text-bold text-3xl border-b-4 border-yellow-500">台灣各地區總確診人數</h2>
-    <span class="mr-2 mt-4 text-bold ">更新時間: {{ timecode }}</span>
+      <div class="info flex justify-between ">
+   <h2 class="ml-2 pb-1 font-bold text-3xl border-b-4 border-yellow-500">台灣各地區總確診人數</h2>
+      <h2 class="ml-28  font-bold text-3xl ">本土人數+ <span class="text-red-500 text-5xl"> {{total}}</span></h2>
+      </div>
+    <span class="mr-2 mt-4 font-bold ">更新時間: {{ timecode }}</span>
     </div>
 
 <div class="section grid grid-rows-1 grid-cols-4 gap-20">
@@ -20,11 +23,13 @@
   v-for="(item, i) in data"
   :key="i"
   >
-    <h2 class="text-xl border-b-2 border-black bg-red-300 font-bold  rounded-full  py-2 ">{{item.a03}}</h2>
+    <h2 class=" CountryTitle text-xl border-b-2 border-black bg-yellow-300 font-bold  rounded-full  py-2 "
+    :class="{ 'plusColor': addData[i]}"
+    >{{item.a03}}</h2>
     
     <div class="peopleNum justify-center">
     <h2 class="text-5xl text-blue-700 py-4 font-bold opacity-100 ">{{item.a06}}</h2>
-     <h3  class="plusNum font-bold text-3xl text-red-700 font-mono" v-if="addData[i]">+{{addData[i]}}</h3>
+     <h3  class="plusNum font-bold text-4xl text-red-700 font-mono" v-if="addData[i]">+{{addData[i]}}</h3>
      </div>
 
   </div>
@@ -41,7 +46,7 @@ export default {
             data: [],
             timecode:'',
             addData:[],
-            Data:[],
+            total: 0
         }
     },
   methods: {
@@ -58,9 +63,13 @@ export default {
        const dataAry = Array.from(filterAry)
         console.log("data: ", dataAry)
         const forData = []
-        for(let i=0;i<filterAry.length;i++){
-            forData.push(filterAry[i].a03)  
-        }
+
+       filterAry.forEach((item)=>{
+         return forData.push(item.a03)
+       })
+        // for(let i=0;i<filterAry.length;i++){
+        //     forData.push(filterAry[i].a03)    //change
+        // }
         const SetAry = [...new Set(forData)]
       const findData = []
 for(let i=0;i<SetAry.length;i++){
@@ -78,9 +87,13 @@ self.data.map((element) => {
       self.addData.push(0)
   }
 })
+self.total = self.addData.reduce((acc, cur)=>{
+  return parseInt(acc) + parseInt(cur)
+})
 console.log('data:', self.data)
 console.log('timecode:', self.timecode)
 console.log('plus:', self.addData)
+console.log('total:', self.total)
       } catch (error) {
         console.log('error: ', error)
       }
@@ -103,7 +116,10 @@ button{
 }
 .plusNum{
 position:absolute;
-right: 5%;
-top: 50%;
+right: 8%;
+top: 45%;
+}
+.plusColor{
+  background: #FF2D2D;
 }
 </style>
