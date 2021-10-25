225e76a1282388d9776598a33da97f76d639370c
Becasuse-hung <4a33a006@stust.edu.tw>
2021-10-25 12:18:42 +0800

    heroku deploy

--- nuxt.config.js
+++ nuxt.config.js
@@ -69,7 +69,6 @@ export default {
     port: 8080, // default: 3000     
     host: '0.0.0.0', // default: localhost   
   },
-
   // Build Configuration: https://go.nuxtjs.dev/config-build
   build: {   
   },
