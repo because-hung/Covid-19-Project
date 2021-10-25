6437e625430a9369a3d131bea19521e20c6b2a7f
Becasuse-hung <4a33a006@stust.edu.tw>
2021-10-25 12:39:31 +0800

    fix bug

--- nuxt.config.js
+++ nuxt.config.js
@@ -65,10 +65,10 @@ export default {
     //   ]
     // }]
   ],
-  server: {     
-    port: 8080, // default: 3000     
-    host: '0.0.0.0', // default: localhost   
-  },
+  // server: {     
+  //   port: 8080, // default: 3000     
+  //   host: '0.0.0.0', // default: localhost   
+  // },
   // Build Configuration: https://go.nuxtjs.dev/config-build
   build: {   
   },
